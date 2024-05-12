6e9391929febb2b6093e909f868b679d9ae57660
EmelyChhu <emelychhu@gmail.com>
2024-05-12 16:02:12 -0400

    Finish Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let result = []
 
+    main: for (let element of array) {
+        for (let argument of remove) {
+            if (element === argument) {
+                continue main;
+            }
+        }
+        
+        result.push(element)
+    }
+
+    return result;
 };
 
 // Do not edit below this line
